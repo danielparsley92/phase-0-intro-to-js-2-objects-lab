daf684491a56d019fee953995fb1de9f16180c06
Daniel Parsley <danielparsley92@gmail.com>
2021-03-31 13:34:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Daniel',
+    streetAddress: '85 Fairview Ave'
+}
+
+function UpdateEmployeeWithKeyAndValue(employee){
+return (employee.name);
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name){
+return delete [employee.key]
+}
+
+function deleteFromEmployeeByKey(){
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+
+}
